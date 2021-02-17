c7bb430e1797c6a54cd25876bbc5d9b5934c1584
bocolo <bronaghcolt@hotmail.com>
2021-02-17 20:26:46 +0000

    added redux packages, provider to index. next to do root-reducer

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; 
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 /*import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 */
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-         </BrowserRouter >
+    <Provider>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter >
+   
 
-  </React.StrictMode>,
+        </React.StrictMode>
+    </Provider>,
   document.getElementById('root')
 );
 
