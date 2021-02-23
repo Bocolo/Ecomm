2d10986235636fb4b10107a50a45e4020842830d
bocolo <bronaghcolt@hotmail.com>
2021-02-23 00:31:25 +0000

    need to correct errors, internet donw

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -9,3 +9,11 @@ export const addItem = item => ({
     payload:item
 
 });
+export const clearFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
+export const removeAction = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload:item
+})
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -11,13 +11,24 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
-                hidden : !state.hidden
-            }
+                hidden: !state.hidden
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id)
+            };
         case CartActionTypes.ADD_ITEM:
             return {
 
                 ...state,
                 cartItems: addItemToCart(state.cartItems,action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: 
             };
         default:
             return state;
