3eacfacb15ff81516a210d491392e6645741f703
bocolo <bronaghcolt@hotmail.com>
2021-04-07 17:49:22 +0100

    Added sign in with email saga, changes google/emailsignIncuccess/failure actions to simple signinsuccess/failure.  refactor: Add getsnapfrom auth to signinwithgoogle/email. updated types/reducer.updated app.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component.jsx';
 import SignInSignUp from './components/signin-and-register/signin-register.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
+
 import { selectCurrentUser } from './redux/user/user.selectors';
 import './App.css';
 
@@ -23,7 +22,7 @@ class App extends React.Component {
     
     unsubscribeFromAuth = null; 
     componentDidMount() {
-        const { setCurrentUser} = this.props;
+        
         /*this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
@@ -80,11 +79,7 @@ const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
 
-});
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+    mapStateToProps
 )(App);
--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
+//import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
 import './signin.styles.scss';
-import { googleSignInStart } from '../../redux/user/user.actions';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -17,14 +17,18 @@ class SignIn extends React.Component {
     }
     handleSubmit =async event => {
         event.preventDefault();
+        const { emailSignInStart } = this.props;
         const { email, password } = this.state;
-        try {
+
+        emailSignInStart( email, password);
+
+       /* try {
             await auth.signInWithEmailAndPassword(email, password);
             this.setState({ email: '', password: '' });
         }
         catch (error) {
             console.log('error handling submit on sign in : ', error.message, error);
-        }
+        }*/
 
 
 
@@ -67,6 +71,8 @@ class SignIn extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-    googleSignInStart: () => dispatch(googleSignInStart())
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) =>
+        dispatch(emailSignInStart({ email, password }))
 })
 export default connect(null, mapDispatchToProps)(SignIn);
